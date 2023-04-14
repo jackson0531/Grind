da662142360fc17d9db1892a83d7c1816a1efa6f
MaTasty <129721767+MaTasty@users.noreply.github.com>
2023-04-13 23:40:22 -0700

    Create MinStack.py

---
+++ MinStack.py
@@ -0,0 +1,45 @@
+# design stack that supports push pop top 
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        if not self.stack:
+            self.stack.append((val, val))
+        else: 
+            m = min(self.stack[-1][1], val)
+            self.stack.append((val, m))
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][0]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
