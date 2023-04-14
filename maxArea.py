1c0516c42f3aa3d8ac0377fcc3d8bb2f57b0565e
MaTasty <129721767+MaTasty@users.noreply.github.com>
2023-04-13 22:31:36 -0700

    Create maxArea.py

---
+++ maxArea.py
@@ -0,0 +1,20 @@
+# given an arr, find two lines that hold the most water
+# return the max amount of water 
+
+# take the bigger one, l or r, and cal max
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(height)-1
+        ans = 0
+        while l<r: 
+            area = (r-l)*min(height[l], height[r])
+            ans = max(ans, area)
+            if height[l]>height[r]:
+                r-=1
+            else:
+                l+=1
+        return ans
